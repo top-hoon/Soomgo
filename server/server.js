60761dda439ed8f145f464da7a708c7041391376
dudgns <dudgns334@gmail.com>
2021-12-22 19:17:14 +0900

    nodemon 설치

--- server/server.js
+++ server/server.js
@@ -4,11 +4,9 @@ const app = express();
 const faq = require('./routes/faq');
 const notice = require('./routes/notice');
 
-
 app.use(faq);
 app.use(notice);
 
 app.listen(3000, () => {
     console.log("3000번 포트로 서버 동작중 ...");
 });
-

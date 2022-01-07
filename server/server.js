598a43eb496390ca73d44d2da0552506453fb0a7
dudgns <dudgns334@gmail.com>
2022-01-07 14:29:11 +0900

    임시 커밋

--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ const app = express();
 
 const faq = require('./routes/faq');
 const notice = require('./routes/notice');
-const member = require('./routes/member');
-const memberjwt = require('./routes/memberjwt');
+// const member = require('./routes/member');
+// const memberjwt = require('./routes/memberjwt');
 const category1 = require('./routes/category1');
 const category2 = require('./routes/category2');
-const category = require('./routes/category');
-const cateTilte = require('./routes/category_question_titile');
+// const category = require('./routes/category');
+// const cateTilte = require('./routes/category_question_titile');
 
 app.use(faq);
 app.use(notice);
 // app.use(member);
-app.use(memberjwt);
+// app.use(memberjwt);
 app.use(category1);
 app.use(category2);
 // const request = require('./routes/request');
@@ -21,9 +21,9 @@ app.use(category2);
 
 app.use(faq);
 app.use(notice);
-app.use(member);
-app.use(category);
-app.use(cateTilte);
+// app.use(member);
+// app.use(category);
+// app.use(cateTilte);
 // app.use(request);
 // app.use(membersession);
 
